02dcf27b52c08121453cc8c718652b8cc9b3a206
Roman Gemini <roman@homefs.biz>
2016-09-30 09:51:59 +0300

    add tests and solution

---
+++ src/AbsolutePath.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 9/30/16
+ * Time: 8:49 AM
+ */
+
+namespace Kote\Utils\AbsolutePath;
+
+/**
+ * Make function that converts relative paths to absolute paths.
+ *
+ * @param string $absoluteRootPath
+ * @return \Closure ($relativePath: string) -> absolutePath: string
+ * @throws \Exception
+ */
+function absolutePathMaker($absoluteRootPath)
+{
+    $isRootDir = function ($location) {
+        return $location == '/';
+    };
+
+    $isAbsolutePath = function ($location) {
+        return strlen($location) > 0 && $location[0] == '/';
+    };
+
+    if (!$isAbsolutePath($absoluteRootPath)) {
+        throw new \Exception("Path '$absoluteRootPath' is not absolute, but must.");
+    }
+
+    $step = function ($location, $node) use ($isRootDir) {
+        if ($node == '.' || $node == '') {
+            return $location;
+        } if ($node == '..') {
+            return $isRootDir($location) ? $location : pathinfo($location, PATHINFO_DIRNAME);
+        }
+        return $isRootDir($location) ?  $location . $node : $location . DIRECTORY_SEPARATOR . $node;
+    };
+
+    return function ($path) use ($isAbsolutePath, $absoluteRootPath, $step) {
+        if ($isAbsolutePath($path)) {
+            throw new \Exception("Path '$path' is absolute, but must not.");
+        }
+
+        $nodes = explode(DIRECTORY_SEPARATOR, $path);
+
+        return array_reduce($nodes, $step, $absoluteRootPath);
+    };
+}
---
+++ tests/AbsolutePathTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 9/30/16
+ * Time: 9:03 AM
+ */
+
+namespace tests;
+
+use function Kote\Utils\AbsolutePath\absolutePathMaker;
+use PHPUnit\Framework\TestCase;
+
+class AbsolutePathTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    private $absoluteRootPath = '/my/root';
+
+    private $pathMaker;
+
+    public function setUp()
+    {
+        $this->pathMaker = absolutePathMaker($this->absoluteRootPath);
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testOneException()
+    {
+        call_user_func($this->pathMaker, '/absolute/path');
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testSecondException()
+    {
+        absolutePathMaker('not/absolute/path');
+    }
+
+    /**
+     * @dataProvider dataProvider
+     * @param $expected
+     * @param $relativePath
+     */
+    public function testPathMaker($expected, $relativePath)
+    {
+        $this->assertEquals($expected, call_user_func($this->pathMaker, $relativePath));
+    }
+
+
+
+    /**
+     * @return array
+     */
+    public function dataProvider()
+    {
+        return [
+            ['/my/root',                    '.'],
+            ['/my',                         '..'],
+            ['/over-root',                  '../../../../over-root'],
+            ['/my/root/some/other/file',    'some/other/file'],
+            ['/my/root/some/other',         'some/forward/..//other']
+        ];
+    }
+}
